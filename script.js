0de906ccb9f236219bf2be9a174108b4bcfe062e
gokulgovinthan <goodgovinthan@gmail.com>
2024-08-13 20:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
-    //Write your code here
+    const x=document.getElementById('form1')
+
+	const name1=document.getElementById('fname').value;
+	const name2=document.getElementById('lname').value;
+
+	alert(name1+ ` `+name2)
 
 }
